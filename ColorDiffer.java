a2e578724507a3e42609164d78c6c389ace7bf9e
sarah lefebvre <bluetechgirl@users.noreply.github.com>
2015-11-11 15:20:53 -0600

    Update ColorDiffer.java

--- ColorDiffer.java
+++ ColorDiffer.java
@@ -19,6 +19,8 @@ ColorSensor sensorRGB;
 DeviceInterfaceModule cdim;
 string Opmode;
 
+
+
         ColorFunction(String color){
 
                 // i think this is how we get the color from the light sensor
@@ -29,6 +31,25 @@ string Opmode;
                 float green = sensorRGB.green();
                 float blue = sensorRGB.blue();
 
+                // i still need to pass the color variables to the functions
+                if(Opmode == "blue") 
+                {
+                        blue_botton();
+                }
+                
+                else if(Opmode == "red")
+                {
+                        red_botton();
+                }
+                
+                else
+                {
+                        // here is where we will print an error to the system log
+                }
+        }
+        
+        blue_botton()
+        {
                 // not complete. I still need to figure out how to check the values
                 if(blue > 150 && red < 25) 
                 {
@@ -44,6 +65,25 @@ string Opmode;
                 {
                         // here is where we will print an error to the system log
                 }
+        }
+
+        red_botton()
+        {
+                // not complete. I still need to figure out how to check the values
+                if(blue > 150 && red < 25) 
+                {
+                        
+                }
                 
+                else if(red > 150 && blue < 25)
+                {
+                        
+                }
+                
+                else
+                {
+                        // here is where we will print an error to the system log
+                }
         }
 }
+
